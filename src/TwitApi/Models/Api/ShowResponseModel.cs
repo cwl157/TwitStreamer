3edfe70f510999dfba92c4459d6e220cbb6db45b
clayton <clayton@LAYTONPC.attlocal.net>
2017-04-02 15:48:33 -0500

    Start separate api Library

---
+++ src/TwitApi/Models/Api/ShowResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitApi.Api.Models
+{
+    internal class ShowResponseModel
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+        public string Description { get; set; }
+    }
+}
