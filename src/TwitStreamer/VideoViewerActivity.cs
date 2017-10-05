72e445e27f775ed47a1cc5a76e86e558fc65bd39
Carl Layton <cwl157@gmail.com>
2017-10-04 20:28:40 -0500

    Issue 8. Video starts playing again after phone is locked

--- src/TwitStreamer/VideoViewerActivity.cs
+++ src/TwitStreamer/VideoViewerActivity.cs
@@ -14,7 +14,7 @@ using TwitStreamer.Infrastructure;
 
 namespace TwitStreamer
 {
-    [Activity(Label = "VideoViewerActivity", ScreenOrientation =Android.Content.PM.ScreenOrientation.Landscape)]
+    [Activity(Label = "VideoViewerActivity", ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape, ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
     public class VideoViewerActivity : Activity
     {
         private const string SAVE_FILE_NAME = "PlayBackData";
@@ -54,6 +54,11 @@ namespace TwitStreamer
             // Create your application here
         }
 
+        protected override void OnRestart()
+        {
+            base.OnRestart();
+        }
+
         protected override void OnStart()
         {
             base.OnStart();
@@ -62,9 +67,11 @@ namespace TwitStreamer
 
         protected override void OnStop()
         {
-            _videoPlayer.Prepared -= OnVideoPlayerPrepared;
-            _mediaController.Dispose();
-            _videoPlayer.Dispose();
+                 _videoPlayer.Prepared -= OnVideoPlayerPrepared;
+            //    _mediaController.Dispose();
+            //    _videoPlayer.Dispose();
+            // TODO: Possibly save position here
+            _videoPlayer.StopPlayback();
             base.OnStop();
         }
 
@@ -73,6 +80,20 @@ namespace TwitStreamer
             _mediaController.Dispose();
             _videoPlayer.Dispose();
             base.OnDestroy();
+        }
+
+        public override void OnConfigurationChanged(Android.Content.Res.Configuration newConfig)
+        {
+            base.OnConfigurationChanged(newConfig);
+            //ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape;
+            //newConfig.Orientation = Android.Content.Res.Orientation.Landscape;
+            // Do something to _videoPlayer so prepared gets called
+            // Super hacky. The only time OnCofigurationChanged is called is when coming and going from the lock screen
+            // so when going to the lock screen back out to the EpisodeDetailsActivity.
+            // This will launch the EpisodeDetailsActivity just to hide it which is a huge waste of resources (I think)
+            // But then coming back from the lock screen, the episode details activity will show and then the user can pick to continue watching the episode.
+            Intent i = new Intent(this.ApplicationContext, typeof(EpisodeDetailsActivity));
+            StartActivity(i);
         }
 
         private void OnVideoPlayerPrepared(object sender, EventArgs e)
